d12717fb25217e0714233dbcc359391f891ec9e9
vicsurcor <vdsurcor@gmail.com>
2024-10-31 16:27:54 +0100

    Ejercicio_3

---
+++ UT2/Ej3/Program.cs
@@ -0,0 +1,48 @@
+﻿//Modify the next program in order to print the word "Hello". Do Not repeat the letters many times!
+ 
+using System;
+using System.Threading;
+namespace ConsoleApp5
+{
+    class Program
+    {
+       
+        public static void Main()
+        {
+            //Thread.CurrentThread.Priority = ThreadPriority.Highest;
+
+            Thread t0 = new Thread(new ThreadStart(WriteO));
+            //t0.Priority = ThreadPriority.AboveNormal
+            Thread t = new Thread(new ThreadStart(WriteH));
+            //t.Priority = ThreadPriority.Normal
+            Thread t1 = new Thread(new ThreadStart(WriteA));
+            //t1.Priority = ThreadPriority.BelowNormal;
+            t0.Start();
+            t.Start();
+            t1.Start();
+
+            t0.Join();
+            t.Join();
+            t1.Join();
+
+        }
+
+        public static void WriteO()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("O");
+               
+            }
+        }
+        public static void WriteH()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("H");
+ 
+            }
+        }
+      
+    }
+}

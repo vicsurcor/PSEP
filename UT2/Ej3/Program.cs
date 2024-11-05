91092255438fcd75eed77bdb88744f63a10a31ef
vicsurcor <vdsurcor@outlook.com>
2024-11-05 04:00:45 +0100

    Ejercicio_3!

--- UT2/Ej3/Program.cs
+++ UT2/Ej3/Program.cs
@@ -1,45 +1,59 @@
 ﻿//Modify the next program in order to print the word "Hello". Do Not repeat the letters many times!
- 
+
 using System;
 using System.Threading;
+
 namespace ConsoleApp5
 {
     class Program
     {
-       
+        private static string word = "";
+        private static readonly string hello = "HELLO";
+        private static readonly object lockObject = new object();
+
         public static void Main()
         {
-            //Thread.CurrentThread.Priority = ThreadPriority.Highest;
+            Thread[] threads = new Thread[5];
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(Write));
+                threads[i].Start();
+            }
+            
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Join();
+            }
 
-            Thread t0 = new Thread(new ThreadStart(WriteO));
-            //t0.Priority = ThreadPriority.AboveNormal
-            Thread t = new Thread(new ThreadStart(WriteH));
-            //t.Priority = ThreadPriority.Normal
-            Thread t1 = new Thread(new ThreadStart(WriteL));
-            Thread t2 = new Thread(new ThreadStart(WriteE));
-            //t1.Priority = ThreadPriority.BelowNormal;
-            t0.Start();
-            t.Start();
-            t1.Start();
-
+            Console.WriteLine("Final result: " + word);
         }
 
-        public static void WriteO()
+        public static void Write()
         {
-            
+            while (true)
+            {
+                bool lockAcquired = false;
+                try
+                {
+                    lockAcquired = Monitor.TryEnter(lockObject);
+                    if (lockAcquired)
+                    {
+                        if (word.Length >= hello.Length)
+                        {
+                            break;
+                        }
+                        word += hello[word.Length];
+                        Console.WriteLine("Thread {0} added a letter. Word: {1}", Thread.CurrentThread.ManagedThreadId, word);
+                    }
+                }
+                finally
+                {
+                    if (lockAcquired)
+                    {
+                        Monitor.Exit(lockObject);
+                    }
+                }
+            }
         }
-        public static void WriteH()
-        {
-            
-        }
-        public static void WriteL()
-        {
-            
-        }
-        public static void WriteE()
-        {
-            
-        }
-      
     }
 }

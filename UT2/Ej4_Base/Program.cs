367adcdf452a4ab22afc181f36c1e10d236ea90d
vicsurcor <vdsurcor@gmail.com>
2024-11-07 16:18:38 +0100

    Ejercicio_4

---
+++ UT2/Ej4_Base/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+
+namespace CountNumber{
+
+   class Program{
+        static int count=0;
+        static void Main(string[] args)
+        {
+            CountANumber();
+            CountANumber();
+            CountANumber();
+            CountANumber();
+            CountANumber();
+            Console.WriteLine($"Count is {count}");
+        }
+
+        public static void CountANumber(){
+            for(int i=1;i<=10000000;i++) count++;              
+        }
+    }
+}

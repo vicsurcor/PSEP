7063db99a3507c64a91fa6656a5d0ba6c7c4df14
vicsurcor <vdsurcor@gmail.com>
2024-11-28 15:24:14 +0100

    UT3.1 2

---
+++ UT3/1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using System.Net;
+
+namespace ComunicacionPrimerosPaso
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            IPHostEntry ipHostInfo = Dns.GetHostEntry("www.google.es");
+
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+
+            Console.WriteLine("google address is: {0}",ipAddress.ToString());
+
+            ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+
+            ipAddress = ipHostInfo.AddressList[0];
+
+            Console.WriteLine("The address of this machine is : {0}", ipAddress);
+
+}
+
+}
+
+}

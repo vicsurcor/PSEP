bb498141761c87931256d0a131b348db8c58b7b7
vicsurcor <vdsurcor@gmail.com>
2024-12-12 16:27:26 +0100

    Binary Serialization

---
+++ UT3/4/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace serialization
+{
+    [Serializable]
+    public class Message : ISerializable
+    {
+        public string Txt { get; set; }
+        public string Hash { get; set; }
+        public DateTime Stamp { get; set; }
+        public Message() { }
+        public Message(string txt, string hash)
+        {
+            this.Txt = txt;
+            this.Hash = hash;
+            Stamp = DateTime.Now;
+        }
+        public Message(SerializationInfo info, StreamingContext context)
+        {
+            Txt = info.GetString("txt");
+            Hash = (string)info.GetValue("hash", typeof(string));
+            Stamp = (DateTime)info.GetValue("stamp", typeof(DateTime));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("txt", Txt);
+            info.AddValue("hash", Hash);
+            info.AddValue("stamp", Stamp);
+        }
+    }
+
+    public static class Program
+    {
+        static void Main()
+        {
+            string fileName = "message.data";
+
+            // Use a BinaryFormatter or SoapFormatter.
+            IFormatter formatter = new BinaryFormatter();
+            //IFormatter formatter = new SoapFormatter();
+
+            Program.SerializeItem(fileName, formatter); // Serialize an instance of the class.
+            Program.DeserializeItem(fileName, formatter); // Deserialize the instance.
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        public static void SerializeItem(string fileName, IFormatter formatter)
+        {
+            // Create an instance of the type and serialize it.
+            Message m = new Message("Hola mundo!", "DAAEF2");
+
+            FileStream s = new FileStream(fileName, FileMode.Create);
+            formatter.Serialize(s, m);
+            s.Close();
+        }
+
+        public static void DeserializeItem(string fileName, IFormatter formatter)
+        {
+            FileStream s = new FileStream(fileName, FileMode.Open);
+            Message m = (Message)formatter.Deserialize(s);
+            Console.WriteLine(m.Txt);
+        }
+    }
+}
--- UT3/4/obj/Debug/net8.0/4.AssemblyInfo.cs
+++ UT3/4/obj/Debug/net8.0/4.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Cliente")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9243e50ded5e16e2ceac680ee903c3721800116b")]
-[assembly: System.Reflection.AssemblyProductAttribute("Cliente")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Cliente")]
+[assembly: System.Reflection.AssemblyProductAttribute("4")]
+[assembly: System.Reflection.AssemblyTitleAttribute("4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.

749f2ccd5ea69b9cbe940b49a6a9ed29c00ce07d
vicsurcor <vdsurcor@gmail.com>
2025-01-09 16:55:34 +0100

    node chat

--- UT3/node/2/client.js
+++ UT3/node/2/client.js
@@ -3,6 +3,11 @@
 const WebSocket = require('ws')
 const url = 'ws://localhost:8080'
 const connection = new WebSocket(url)
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
  
 connection.onopen = () => {
   connection.send('Message From Client') 
@@ -14,4 +19,9 @@ connection.onerror = (error) => {
  
 connection.onmessage = (e) => {
   console.log(e.data)
+  rl.on('line', (answer) => {
+    connection.send(answer)
+  }
+  );
+  rl.close();
 }

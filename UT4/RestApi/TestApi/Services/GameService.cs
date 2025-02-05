3f1bf4eb76ae8e58d23a1a626257555c857430f6
vicsurcor <vdsurcor@outlook.com>
2025-02-05 17:31:27 +0100

    RestApi-Services_GameAutoIncrement

--- UT4/RestApi/TestApi/Services/GameService.cs
+++ UT4/RestApi/TestApi/Services/GameService.cs
@@ -5,10 +5,12 @@ using Newtonsoft.Json;
 public class GameService
 {
     public List<Game> Games { get; private set; } = new List<Game>();
+    private int _nextGameId;
 
     public GameService()
     {
         LoadData();
+        InitializeIds();
     }
 
     private void LoadData()
@@ -19,5 +21,22 @@ public class GameService
             string jsonData = File.ReadAllText(filePath);
             Games = JsonConvert.DeserializeObject<List<Game>>(jsonData) ?? new List<Game>();
         }
+    }
+     // Initialize the next available GameId
+    private void InitializeIds()
+    {
+        if (Games.Any())
+        {
+            _nextGameId = Games.Max(g => g.Id) + 1; // Start after the highest Game ID
+        }
+        else
+        {
+            _nextGameId = 1;
+        }
+    }
+    // Method to get the next Game ID
+    public int GetNextGameId()
+    {
+        return _nextGameId++;
     }
 }

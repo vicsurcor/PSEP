3f9b43cfc297402b2cc7b43d31097c967ddda059
vicsurcor <vdsurcor@outlook.com>
2025-02-20 19:55:09 +0100

    CryptoUsages-AES

--- UT5/CryptoUsages/unit-testing-decoder-main/unit-testing-decoder-main/CryptoLib/X.cs
+++ UT5/CryptoUsages/unit-testing-decoder-main/unit-testing-decoder-main/CryptoLib/X.cs
@@ -9,7 +9,7 @@ namespace CryptoLib
 {
     public class X
     {
-
+        private const int KeyLength = 32; // AES-256 requires a 32-byte key
         public static string RsaGetPubParsXml(RSACryptoServiceProvider rsa)
         {
             bool isPriv = false;
@@ -85,12 +85,43 @@ namespace CryptoLib
 
         public static string AesEncrypt(string msg, string pwd, out string iv)
         {
-            iv = "";
-            return null;
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = GetKey(pwd);
+                aes.GenerateIV();
+                iv = Convert.ToBase64String(aes.IV);
+
+                using (var encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+                using (var ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    using (var sw = new StreamWriter(cs))
+                    {
+                        sw.Write(msg);
+                    }
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
         }
+
         public static string AesDecrypt(string enc, string pwd, string sal)
         {
-            return null;
+            var cipherBytes = Convert.FromBase64String(enc);
+            var iv = Convert.FromBase64String(sal);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = GetKey(pwd);
+                aes.IV = iv;
+
+                using (var decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+                using (var ms = new MemoryStream(cipherBytes))
+                using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                using (var sr = new StreamReader(cs))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
         }
 
         public static string ShaHash(Object input)
@@ -142,11 +173,27 @@ namespace CryptoLib
 
             // Return the hexadecimal string.
             return sBuilder.ToString();
+        }
+
+        private static byte[] GetKey(string pwd)
+        {
+            if (pwd.Length < KeyLength)
+            {
+                // Pad the password with zeroes
+                pwd = pwd.PadRight(KeyLength, '0');
+            }
+            else if (pwd.Length > KeyLength)
+            {
+                // Truncate the password
+                pwd = pwd.Substring(0, KeyLength);
+            }
+
+            return Encoding.UTF8.GetBytes(pwd);
         }   
+        
 
     }
     
-
     public class Utf8StringWriter : StringWriter
     {
         public override Encoding Encoding => Encoding.UTF8;

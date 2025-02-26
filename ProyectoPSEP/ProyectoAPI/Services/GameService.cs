a710ad009ba6f47554fac7951877873c7f8d6a23
vicsurcor <vdsurcor@outlook.com>
2025-02-26 14:34:49 +0100

    ProyectoPSEP-Added Services

---
+++ ProyectoPSEP/ProyectoAPI/Services/GameService.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+public class GameService
+{
+    public string path = "Datasets/";
+    public List<Game> Games { get; private set; } = new List<Game>();
+    private int _nextGameId;
+
+    public GameService()
+    {
+        LoadData();
+        InitializeIds();
+    }
+
+    private void LoadData()
+    {
+        string filePath = path + "test-data-game.json";
+        if (File.Exists(filePath))
+        {
+            string jsonData = File.ReadAllText(filePath);
+            Games = JsonConvert.DeserializeObject<List<Game>>(jsonData) ?? new List<Game>();
+        }
+    }
+     // Initialize the next available GameId
+    private void InitializeIds()
+    {
+        if (Games.Any())
+        {
+            _nextGameId = Games.Max(g => g.Id) + 1; // Start after the highest Game ID
+        }
+        else
+        {
+            _nextGameId = 1;
+        }
+    }
+    // Method to get the next Game ID
+    public int GetNextGameId()
+    {
+        return _nextGameId++;
+    }
+}
---
+++ ProyectoPSEP/ProyectoAPI/Services/UserService.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+public class UserService
+{
+    public string path = "Datasets/";
+    public List<User> Users { get; private set; } = new List<User>();
+    private int _nextGameId;
+
+    public UserService()
+    {
+        LoadData();
+        InitializeIds();
+    }
+
+    private void LoadData()
+    {
+        string filePath = path + "test-data-user.json";
+        if (File.Exists(filePath))
+        {
+            string jsonData = File.ReadAllText(filePath);
+            Users = JsonConvert.DeserializeObject<List<User>>(jsonData) ?? new List<User>();
+        }
+    }
+     // Initialize the next available UserId
+    private void InitializeIds()
+    {
+        if (Users.Any())
+        {
+            _nextUserId = Users.Max(g => g.Id) + 1; // Start after the highest User ID
+        }
+        else
+        {
+            _nextUserId = 1;
+        }
+    }
+    // Method to get the next Game ID
+    public int GetNextUserId()
+    {
+        return _nextUserId++;
+    }
+}

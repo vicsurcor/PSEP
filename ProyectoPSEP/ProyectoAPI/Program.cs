8806c8bc45dddb6f03d01f9f0f3fabca30b865e7
vicsurcor <vdsurcor@outlook.com>
2025-03-05 16:34:32 +0100

    /

--- ProyectoPSEP/ProyectoAPI/Program.cs
+++ ProyectoPSEP/ProyectoAPI/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-//TODO : Test and Fix Clients
 //TODO : Roles
 //TODO : Firebase Integration
 //TODO : Video Demo
